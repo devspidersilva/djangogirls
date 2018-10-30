0eb62daffb26b85f540ceea9231bbc186228a977
Anderson Silva <anderson1994_fsa@hotmail.com>
2018-10-30 01:24:49 -0200

    tentando normalizar

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'z4vn69a6jwctsya&mdgld@3da7slp7=b+p*xe0c#0whto&6ey1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
